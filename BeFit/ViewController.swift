c6dddcf94840145ad891b14d6715654ca28ff915
Student1 <student1@lab4-17.local>
2017-09-05 09:58:08 +0530

    Initial Commit

---
+++ BeFit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeFit
+//
+//  Created by Student1 on 05/09/17.
+//  Copyright © 2017 Student1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
